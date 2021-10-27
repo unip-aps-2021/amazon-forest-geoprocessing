31b2f2aa3dccd3e237065b86b859eefd1049bbab
Michael Alcantara <michael.vergil@hotmail.com>
2021-10-26 23:00:31 -0300

    Primeiro Git

---
+++ APS.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/*
+Tela de introdução explicando o projeto
+	Opções de Iniciar ordenação / comparar os 3 tipos de ordenação / sair
+Tela de iniciar ordenação
+	Opções de iniciar / pesquisar dado específico / sair
+Tela de comparar
+	Opções de iniciar comparação / sair
+*/
+
+int main()
+{
+    printf("############## Sistema de controle do Geoprocessamento da Floresta Amazonica  ##############\n\n");
+    printf("Esse programa tem como objetivo processar as imagens geradas pelos satélites da floresta ");
+    
+    printf("BBBBB");
+}
